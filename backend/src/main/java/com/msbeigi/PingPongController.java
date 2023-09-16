5c01794d99ed14ecd4653d167e75d4288d9aa87e
msbeigiai <msbeigi83@gmail.com>
2023-09-16 18:34:17 +0300

    CD workflow with new controller

---
+++ backend/src/main/java/com/msbeigi/PingPongController.java
@@ -0,0 +1,14 @@
+package com.msbeigi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+    record PingPong(String result) {}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong");
+    }
+}
