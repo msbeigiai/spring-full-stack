d965434fc09fc064f23d4e3f736a0ced766510a9
msbeigiai <msbeigi83@gmail.com>
2023-09-17 12:26:40 +0300

    Ping pong controller editd

--- backend/src/main/java/com/msbeigi/PingPongController.java
+++ backend/src/main/java/com/msbeigi/PingPongController.java
@@ -11,6 +11,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong: " + ++COUNTER);
+        return new PingPong("Number of Pong: " + ++COUNTER);
     }
 }
