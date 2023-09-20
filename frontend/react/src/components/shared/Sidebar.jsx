8e570d9190d1c7082a7030a1b1072ccfc818a8c8
msbeigiai <msbeigi83@gmail.com>
2023-09-20 10:38:30 +0300

    Change navigation color - 3

--- frontend/react/src/components/shared/Sidebar.jsx
+++ frontend/react/src/components/shared/Sidebar.jsx
@@ -81,7 +81,7 @@ const NavItem = ({ icon, children, ...rest }) => {
                 role="group"
                 cursor="pointer"
                 _hover={{
-                    bg: "blue.300",
+                    bg: "red.300",
                     color: "white"
                 }}
                 {...rest}
