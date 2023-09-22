dff31cf18a7dc3bee9d4a88876046adf56b44df3
msbeigiai <msbeigi83@gmail.com>
2023-09-22 13:25:37 +0300

    Expose health and info metrics and add application-dev.yaml

--- backend/src/main/java/com/msbeigi/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/msbeigi/security/SecurityFilterChainConfig.java
@@ -43,6 +43,8 @@ public class SecurityFilterChainConfig {
                         .permitAll()
                         .requestMatchers(HttpMethod.GET, "/ping")
                         .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/**")
+                        .permitAll()
                         .anyRequest()
                         .authenticated()
                 )
